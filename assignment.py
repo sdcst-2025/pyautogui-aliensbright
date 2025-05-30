abf268f4188d0724b3b9cc2697fc5d27a2009b2e
aliensbright <144503232+aliensbright@users.noreply.github.com>
2025-05-30 11:34:03 -0700

    hello

--- assignment.py
+++ assignment.py
@@ -22,6 +22,7 @@ def startGame():
         print('Game in FullScreen')
     except:
         print('Fullscreen button not detected,\nplease ensure that the game has loaded.')
+    time.sleep(1)
 
 def getmidCoords():#returns the planet coords
     time.sleep(0.5)
@@ -36,64 +37,10 @@ def maintask(xcoord,ycoord,timelimit): #pressing down on the middle of the plane
     newtime=0
     while newtime-startTime<timelimit:
         pyautogui.moveTo(xcoord,ycoord)
-        mouseAutoclickThing()
+        mouseAutoclickThing(xcoord,ycoord)
         newtime=time.time()
 
-""" 
-#def start(): #gets to the main screen
-#    try:
-#        pyautogui.moveTo(pyautogui.locateCenterOnScreen('assets/rungame.png'))
-#        pyautogui.click(clicks=2,interval=0.5)
-#        time.sleep(4)
-#    except:
-#        try:
-#            pyautogui.moveTo(pyautogui.locateCenterOnScreen('assets/sleep.png',confidence=0.8))
-#            pyautogui.click(clicks=2,interval=0.5)
-#            time.sleep(1)
-#        except:
-#            pass
-#
-
-def sidetask1(): #goes to the shop on the bottom of the screen.
-
-    shoplist=['damage','speed','limit','auto']
-    shoplistlength=shoplist
-    n=True
-    while n==True:
-        for i in shoplist:
-            try:
-                pyautogui.moveTo(pyautogui.locateCenterOnScreen(f'assets/{i}.png',confidence=0.8))
-                mousething()
-                time.sleep(0.5)
-            except:
-                shoplistlength.remove(i)
-                if len(shoplistlength)==0:
-                    n=False
-
-#def sidetask3(): #Go into rebirth tab and use feathers for effects
-#    pyautogui.moveTo(pyautogui.locateCenterOnScreen('assets/rebirth.png',confidence=0.9))
-#    pyautogui.click(clicks=2,duration=0.5)
-#    x,y= pyautogui.locateCenterOnScreen('assets/rebirthknife.png',confidence=0.9)
-#    reCoords=[(x,y),(x+75,y),(x+150,y),(x+225,y),(x,y+100),(x+75,y+100),(x+150,y+100),(x+225,y+100)]
-#    for i in reCoords:
-#        pyautogui.moveTo(i[0],i[1],0.1)
-#        pyautogui.click()
-#    pyautogui.moveTo(pyautogui.locateCenterOnScreen('assets/xmarks.png',confidence=0.9))
-#    pyautogui.click(2,duration=0.5)
-
-def sidetask2(): #collects gems from the collection tab
-    n=True
-    while n==True:
-        try:
-            x,y=(pyautogui.locateCenterOnScreen('assets/collection.png', confidence=.7))
-            pyautogui.moveTo(x,y-20)
-            mousething()
-            time.sleep(1)
-        except:
-            pass
-        try:
-            for i in range(6):
-                if i==0:
-                    try:
-                        pyautogui.locateC nterOnScreen('assets/gems.png',region=(x,y-100,300,500),confidence=0.9)
-                    except:           """
+def mouseAutoclickThing(x,y):
+    pyautogui.moveTo(x,y)
+    pyautogui.mouseDown()
+    pyautogui.mouseUp()
